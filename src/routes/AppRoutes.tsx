e8e6da86f111736413667d9291bb0969ef556a11
Marcos Oliveira <108419708+MarcosOliveira7@users.noreply.github.com>
2023-06-29 13:18:43 -0300

    update

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,27 +1,41 @@
 import React from "react";
 
-import { Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Layout from '../components/layout/';
+import Layout from "../components/layout/";
 
-import Home from '../pages/home';
-import CadastrarInformacoes from '../pages/curriculo/CadastrarInformacoes';
-import CadastrarExperiencia from '../pages/curriculo/CadastrarExperiencia/CadastrarExperiencia';
-import ListaPortfolio from '../pages/portfolio/ListaPortfolio';
-import ListaExperiencia from '../pages/curriculo/ListaExperiencia'
-import CadastrarPortfolio from '../pages/portfolio/CadastrarPortfolio';
-
-
+import Home from "../pages/home";
+import CadastrarInformacoes from "../pages/curriculo/CadastrarInformacoes";
+import CadastrarExperiencia from "../pages/curriculo/CadastrarExperiencia/CadastrarExperiencia";
+import ListaPortfolio from "../pages/portfolio/ListaPortfolio";
+import ListaExperiencia from "../pages/curriculo/ListaExperiencia";
+import CadastrarPortfolio from "../pages/portfolio/CadastrarPortfolio";
 
 const AppRoutes: React.FC = () => {
-    return (
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-        <Layout>
-            <Outlet />
-        </Layout>
+        <Route
+          path="/curriculo/informacoes/cadastro"
+          element={<CadastrarInformacoes />}
+        />
 
-    );
+        <Route
+          path="/curriculo/experiencia/cadastro"
+          element={<CadastrarExperiencia />}
+        />
 
+        <Route
+          path="curriculo/experiencia/lista"
+          element={<ListaExperiencia />}
+        />
+        <Route path="/portfolio/cadastro" element={<CadastrarPortfolio />} />
+        <Route path="/portfolio/lista" element={<ListaPortfolio />} />
+      </Routes>
+    </Layout>
+  );
 };
 
 export default AppRoutes;
